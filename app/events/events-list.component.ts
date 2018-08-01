64422ed651129b489bea9a31bfa71398f4a3cef3
Ajay-PC\Ajay <araval@argusoft.com>
2018-08-02 01:08:26 +0530

    Passed data from parent events-list component to child event-thumbnail component.

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector:'events-list',
-    templateUrl:'app/events/events-list.component.html'
+    selector: 'events-list',
+    template: `<div>
+                <h1>Upcoming Angular Events</h1>
+                <hr/>
+                <event-thumbnail [event]="event1"></event-thumbnail>
+            </div>`
 })
-export class EventsListComponent{
-    event= {
-        id:1,
-        name:'Angular Connect',
-        date:'09/26/2036',
-        time:"10:00 AM",
-        price:599.99,
-        imageurl:'app/assets/images/angularconnect-shield.png',
-        location:{
-            address:'1057 DT',
-            city:'London',
-            country:'England'
+export class EventsListComponent {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '09/26/2036',
+        time: "10:00 AM",
+        price: 599.99,
+        imageurl: 'app/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
 
         }
     }
