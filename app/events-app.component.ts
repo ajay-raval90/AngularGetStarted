1af33d329ff8e8cadf64f128e3c137440e2d6a83
Ajay-PC\Ajay <araval@argusoft.com>
2018-08-02 00:54:57 +0530

    Made event list component. User Template Url instead of the inline template. Register Component with AppModule in declarations section

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -1,7 +1,8 @@
 import {Component }from '@angular/core'
+import {EventsListComponent} from './events/events-list.component';
 @Component({
   selector:'events-app',
-  template:`<h1>Angular 2 is up and running</h1>`  
+  template:`<events-list></events-list>`,  
 })
 export class EventsAppComponent
 {
