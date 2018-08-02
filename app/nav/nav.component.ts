548dfa0e47b84a65cde8ea26df11e5499ab5bf3f
Ajay-PC\Ajay <araval@argusoft.com>
2018-08-03 00:13:23 +0530

    Added Nav Bar Component Registered with Appmodule Used it in events app component

---
+++ app/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'nav-bar',
+    templateUrl: 'app/nav/nav.component.html',
+    styles: [
+        `.nav.navbar-nav {font-size:15px;}
+        #seacchForm {margin-right: 100px;}
+        @media(max-width:1200px){
+            #searchForm {
+                display:none;
+            }
+        }
+        `
+
+    ]
+})
+export class NavBarCompnent {
+
+}
