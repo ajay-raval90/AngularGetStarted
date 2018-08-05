95f8a7d0213b7f1056dced10802245696740a0d6
Ajay-PC\Ajay <araval@argusoft.com>
2018-08-05 13:56:28 +0530

    1) Created First Service 2) Registered the service with the module 3) Used in event-list component 4) Implemented ngOnInit method.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,21 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {EventsAppComponent} from './events-app.component';
-import {EventsListComponent} from './events/events-list.component';
-import {EventThumbnailComponent} from './events/event-thumbnail.component';
-import {NavBarCompnent} from './nav/nav.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarCompnent } from './nav/nav.component';
+import { EventService } from './events/shared/event-service';
 @NgModule({
-    imports:[BrowserModule],
-    declarations:[
+    imports: [BrowserModule],
+    declarations: [
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
         NavBarCompnent
-        ],
-    bootstrap:[EventsAppComponent]
+    ],
+    providers: [EventService],
+    bootstrap: [EventsAppComponent]
 })
-export class AppModule{
+export class AppModule {
 
 }
