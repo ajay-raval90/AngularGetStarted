46d7d13fa989eaf0b5d3610f5260bb10a76b17b9
Ajay-PC\Ajay <araval@argusoft.com>
2018-08-10 00:49:47 +0530

    Created event model. Used and export interface and used strinct tyoing for the compile time type safety.

--- app/events/shared/event-service.ts
+++ app/events/shared/event-service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
-import {Subject}from 'rxjs/RX';
+import {Subject,Observable}from 'rxjs/RX';
+import{IEvent} from './index'
 @Injectable()
 export class EventService {
 
-  getEvents() {
-    let subject = new Subject();
+  getEvents() :Observable<IEvent[]>{
+    let subject = new Subject<IEvent[]>();
     setTimeout(function() {
       subject.next(Events);
       subject.complete();
     }, 10);
     return subject;
   }
-  getEvent(id: number) {
+  getEvent(id: number):IEvent {
     return Events.find(t => t.id === id);
   }
 
 }
-const Events = [
+const Events:IEvent[] = [
   {
     id: 1,
     name: 'Angular Connection',
-    date: '9/26/2036',
+    date:new Date('9/26/2036'),
     time: '10:00 am',
     price: 599.99,
     imageUrl: '/app/assets/images/angularconnect-shield.png',
@@ -98,7 +99,7 @@ const Events = [
   {
     id: 2,
     name: 'ng-nl',
-    date: '4/15/2037',
+    date: new Date('4/15/2037'),
     time: '9:00 am',
     price: 950.00,
     imageUrl: '/app/assets/images/ng-nl.png',
@@ -154,7 +155,7 @@ const Events = [
   {
     id: 3,
     name: 'ng-conf 2037',
-    date: '5/4/2037',
+    date: new Date('5/4/2037'),
     time: '9:00 am',
     price: 759.00,
     imageUrl: '/app/assets/images/ng-conf.png',
@@ -236,7 +237,7 @@ const Events = [
   {
     id: 4,
     name: 'UN Angular Summit',
-    date: '6/10/2037',
+    date: new Date('6/10/2037'),
     time: '8:00 am',
     price: 800.00,
     imageUrl: '/app/assets/images/basic-shield.png',
@@ -285,7 +286,7 @@ const Events = [
   {
     id: 5,
     name: 'ng-vegas',
-    date: '2/10/2037',
+    date: new Date('2/10/2037'),
     time: '9:00 am',
     price: 400.00,
     imageUrl: '/app/assets/images/ng-vegas.png',
@@ -310,7 +311,7 @@ const Events = [
       },
       {
         id: 2,
-        name: "Angular 4 in 60ish Minutes",
+        name: "Angular 4 in 60ish Minutes", 
         presenter: "Dan Wahlin",
         duration: 2,
         level: "Beginner",
