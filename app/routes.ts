22c91d5bf899ab96b13a0b9c586372fbf9fd0f27
Ajay-PC\Ajay <araval@argusoft.com>
2018-08-09 23:55:23 +0530

    Created second NgModule (The Feature Module) Define Configuration for the user feature module In Main Module's Routes, Used Path like user and loadchildren : 'app/user/user.module#UserModule'.

--- app/routes.ts
+++ app/routes.ts
@@ -2,16 +2,19 @@ import { Routes } from '@angular/router';
 import { EventsListComponent } from './events/events-list.component';
 import { EventDetailsComponent } from "./events/event-details.component";
 import { Error404Component } from "./errors/404.component";
-import {CreateEventComponent} from './events/create-event.component';
-import {EventRouteActivator} from './events/event-route-activator'
+import { CreateEventComponent } from './events/create-event.component';
+import { EventRouteActivator } from './events/event-route-activator'
 import { EventRouteDeactivator } from './events/event-route-deactivator';
 import { EventListResolver } from "./events/event-list.resolver";
 export const routes: Routes = [
-    { path: 'events/new', component: CreateEventComponent,canDeactivate:[EventRouteDeactivator] },
-    { path: 'events', component: EventsListComponent, resolve:{
-        events : EventListResolver
-    } },
-    { path: 'events/:id', component: EventDetailsComponent, canActivate:[EventRouteActivator] },
-    { path: '404', component: Error404Component  },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: 'events/new', component: CreateEventComponent, canDeactivate: [EventRouteDeactivator] },
+    {
+        path: 'events', component: EventsListComponent, resolve: {
+            events: EventListResolver
+        }
+    },
+    { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+    { path: '404', component: Error404Component },
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
 ];
---
+++ app/user/user.routes.ts
@@ -0,0 +1,5 @@
+import { ProfileComponent } from './profile.component';
+import { Routes } from '@angular/router';
+export const userRoutes: Routes = [
+    { path: 'profile', component: ProfileComponent }
+]
